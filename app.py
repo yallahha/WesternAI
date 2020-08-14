d2d476100e9a1d20e86b45afbb4008550ef4488d
yallahha <35283964+yallahha@users.noreply.github.com>
2020-08-14 18:04:57 -0400

    adddd restrictions to image upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from werkzeug.utils import secure_filename
 import os
 import os.path
@@ -15,10 +15,16 @@ import joblib
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecretkey'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 recogniton_model = load_model("D:\Documents\WesternAI\WesternAI\myModel1.h5")
 
 class photoForm(FlaskForm):
     photo = FileField(validators=[FileRequired()])
+
+def allowed_file(filename):
+    print('hi')
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def predictImg(img):
@@ -48,20 +54,19 @@ def upload_file():
     form = photoForm()
     if request.method == 'POST':
         f = form.photo.data
-        extension = os.path.splitext(f.filename)[1]
-        session['ext'] = extension
-        filename = 'pre-predictImg' + extension #secure_filename(f.filename)
-        path = 'D:\Documents\WesternAI\WesternAI\static\photos'
-        f.save(os.path.join(
-            path, filename
-        ))
-        
-        return redirect(url_for('predict'))
+        if f and allowed_file(f.filename):
+            extension = os.path.splitext(f.filename)[1]
+            session['ext'] = extension
+            filename = 'pre-predictImg' + extension #secure_filename(f.filename)
+            path = 'D:\Documents\WesternAI\WesternAI\static\photos'
+            f.save(os.path.join(
+                path, filename
+            ))
+            return redirect(url_for('predict'))
     return render_template('westernai.html', form = form)
 
 @app.route('/prediction')
 def predict():
-    print(session['ext'])
     img = 'D:\Documents\WesternAI\WesternAI\static\photos\pre-predictImg' + session['ext']
     prediction = predictImg(img)
     return render_template('prediction.html', value = prediction, img = img)
